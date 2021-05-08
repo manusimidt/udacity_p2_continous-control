81a9b48b569d7f0ff58da66613a4fb9f92f8cadf
Manuel Schmidt <manu.schmidt.14@gmail.com>
2021-05-08 11:20:17 +0200

    Added model definition for Actor Network

--- Continous_Control.py
+++ Continous_Control.py
@@ -1,33 +1,46 @@
 from unityagents import UnityEnvironment
 import numpy as np
 
-env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64')
 
-# get the default brain
-brain_name = env.brain_names[0]
-brain = env.brains[brain_name]
+def watch_agent(env: UnityEnvironment, brain_name: str, agent: Agent) -> None:
+    """
+    Shows agent simulation
+    :param env:
+    :param brain_name:
+    :param agent:
+    :return:
+    """
+    env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
+    state = env_info.vector_observations[0]  # get the current state
+    score = 0  # initialize the score
 
-env_info = env.reset(train_mode=True)[brain_name]
+    while True:
+        action = agent.act(state, epsilon=0.05)
+        env_info = env.step(action)[brain_name]  # send the action to the environment
+        next_state = env_info.vector_observations[0]  # get the next state
+        reward = env_info.rewards[0]  # get the reward
+        done = env_info.local_done[0]  # see if episode has finished
+        score += reward  # update the score
+        state = next_state  # roll over the state to next time step
+        if done:  # exit loop if episode finished
+            break
+    print(f"Agent achieved a score of {score}")
 
-num_agents = len(env_info.agents)
-action_size = brain.vector_action_space_size
-states = env_info.vector_observations
-state_size = states.shape[1]
 
-env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
-states = env_info.vector_observations  # get the current state (for each agent)
-scores = np.zeros(num_agents)  # initialize the score (for each agent)
-while True:
-    actions = np.random.randn(num_agents, action_size)  # select an action (for each agent)
-    actions = np.clip(actions, -1, 1)  # all actions between -1 and 1
-    env_info = env.step(actions)[brain_name]  # send all actions to tne environment
-    next_states = env_info.vector_observations  # get next state (for each agent)
-    rewards = env_info.rewards  # get reward (for each agent)
-    dones = env_info.local_done  # see if episode finished
-    scores += env_info.rewards  # update the score (for each agent)
-    states = next_states  # roll over states to next time step
-    if np.any(dones):  # exit loop if episode finished
-        break
-print('Total score (averaged over agents) this episode: {}'.format(np.mean(scores)))
+if __name__ == '__main__':
+    env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64')
 
-env.close()
+    # get the default brain
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+    num_agents = len(env_info.agents)
+    action_size = brain.vector_action_space_size
+    states = env_info.vector_observations
+    state_size = states.shape[1]
+
+    env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
+    states = env_info.vector_observations  # get the current state (for each agent)
+    scores = np.zeros(num_agents)  # initialize the score (for each agent)
+
+    env.close()
--- networks.py
+++ networks.py
@@ -4,13 +4,26 @@ import torch.nn.functional as F
 import numpy as np
 
 
+def hidden_init(layer) -> tuple:
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return -lim, lim
+
+
 class ActorNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size: int, output_size: int, fc1_size: int = 128, fc2_size: int = 128):
         super().__init__()
-        pass
+        self.fc1 = nn.Linear(in_features=input_size, out_features=fc1_size)
+        self.fc2 = nn.Linear(in_features=fc1_size, out_features=fc2_size)
+        self.fc3 = nn.Linear(in_features=fc2_size, out_features=output_size)
 
     def forward(self, x):
         pass
+
+    def reset_weights(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
 
 class CriticNetwork(nn.Module):
@@ -19,4 +32,6 @@ class CriticNetwork(nn.Module):
         pass
 
     def forward(self, x):
-        pass
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.tanh(self.fc3(x))
